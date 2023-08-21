bdeddcffc8f5a2d99699afea1e3b79d56fc68cbb
Meistt <aguslive7@gmail.com>
2023-08-21 20:45:09 -0300

    rompo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export cass AppComponent {
   title = 'test-deploy';
 }
